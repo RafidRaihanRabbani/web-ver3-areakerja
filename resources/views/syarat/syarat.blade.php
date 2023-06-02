3800873d0146b374e73ddc1a973dc56be2dbc67d
RafidRaihanRabbani <96913996+RafidRaihanRabbani@users.noreply.github.com>
2023-06-02 08:38:39 +0700

    Add files via upload

---
+++ resources/views/syarat/syarat.blade.php
@@ -0,0 +1,193 @@
+@extends('user.main')
+@section('meta')
+<meta property="og:type" content="website">
+<meta property="og:url" content="{{ url()->current() }}">
+<meta property="og:title" content=" - Area Kerja">
+<meta property="og:description" content="Platform lowongan kerja no. 1 untuk mendapatkan talenta terbaik bagi perusahaan anda.">
+<meta property="og:image" content="{{ asset('img/img-01(2).png') }}">
+<meta name="description" content="Areakerja adalah perusahaan berbasis teknologi informasi yang berpusat di Yogyakarta. Perusahaan ini berfokus pada platform untuk mencari lowongan kerja di Daerah Istimewa Yogyakarta (DIY).">
+<meta name="keywords" content="Tentang Kami">
+@endsection
+@section('home')
+<section class="relative" id="home">
+    <div class="banner-syarat">
+        <div class="row align-items-center" style="margin-right: 15px; margin-left: -600px; margin-bottom: 250px; margin-top: 30px;">
+            <div class="banner-content col-lg-12">
+                <h1 style="color: #ffffff; font-size: 50px; margin-right: 290px; text-shadow: 2px 2px 3px #353535b0;">
+                    <strong>Syarat</strong>
+                </h1>
+                <h2 style="color: #ffffff; font-size: 50px; margin-bottom:5px; margin-right: 80px; text-shadow: 2px 2px 3px #353535b0;">
+                    <strong>dan Ketentuan</strong>
+                </h2>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+@endsection
+@section('aboutus')
+    <section class="relative" id="home">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10" style="overflow:hidden; color: black">
+                    <h2 style="font-weight: 50px; color: rgb(0, 0, 0); padding-top: 50px" class=" text-left ">Syarat & Ketentuan</h2>
+                    <h2 style="font-weight: 50px; color: rgb(0, 0, 0); padding-bottom: 10px" class=" text-left ">Pengguna</h2>
+                    <p style= "font-size: 15px">
+                        Pada dokumen Syarat dan Ketentuan Penggunaan (“S&K”) ini menyatakan syarat dan ketentuan yang perlu Anda 
+                        penuhi dan setujui untuk dapat menggunakan aplikasi dan/atau situs yang kami sediakan (“Layanan”) dengan segala 
+                        fitur dan kegunaannya, dan hubungan Anda dengan Area Kerja sebagai penyedia Layanan. Harap baca S&K dengan 
+                        seksama karena isinya akan mempengaruhi hak dan kewajiban Anda berdasarkan hukum. Jika Anda tidak menyetujui 
+                        S&K ini, mohon untuk tidak mendaftar atau menggunakan Layanan.
+                    </p>
+                    <p style= "margin-bottom: -2px; font-size: 15px">
+                        Penggunaan Layanan melibatkan beberapa pihak yang masing-masing memiliki hak dan kewajiban yang perlu 
+                        dipenuhi. Pihak-pihak tersebut adalah:
+                    </p>
+
+                    <ul class= "b" style= "margin-left: 20px">
+                        <li class="m-0" style= "font-size: 15px;" >
+                            Area Kerja, sebagai penyedia Layanan.
+                        </li>
+        
+                        <li class="m-0" style= "font-size: 15px" >
+                            Pengguna Layanan (“Pengguna”), yaitu individu yang bertujuan untuk mencari informasi lowongan pekerjaan dan/
+                            atau melamar pekerjaan (“Pencari Kerja”) dan individu atau entitas bisnis yang sedang mencari pekerja atau 
+                            karyawan pada Layanan (“Perusahaan”) serta individu yang menggunakan Layanan untuk mengakses soal-soal 
+                            pada fitur Belajar yang terdapat pada Layanan.
+                        </li>
+                    </ul>
+                </div>
+                
+                <div class="col-lg-10" style="overflow:hidden; color: black">
+                    <h2 style="font-weight: 50px; color: rgb(0, 0, 0); padding-top: 15px; padding-bottom: 5px" class=" text-left ">Ketentuan Umum Pengguna</h2>
+                    <p style= "margin-bottom: -2px; font-size: 15px">
+                        Dengan menggunakan Layanan, Anda menyatakan bahwa:
+                    </p>
+
+                    <ol class= "d" style= "margin-left: 20px">
+                
+                        <li style= "font-size: 15px" >
+                            Anda setuju untuk terikat dengan S&K yang ditetapkan serta kebijakan mengenai pembaruan S&K di kemudian hari. 
+                            Anda dipersilahkan untuk tidak menggunakan dan/atau mengakses Layanan jika tidak setuju untuk terikat dengan 
+                            S&K ini atau pun pembaruannya.
+                        </li>
+
+                        <li style= "font-size: 15px" >
+                            Apabila Anda belum dewasa menurut ketentuan perundang-undangan yang berlaku, wajib mendapatkan 
+                            bimbingan dan pengawasan orang tua dalam menggunakan Layanan.
+                        </li>
+                        <li style= "font-size: 15px" >
+                            Jika ada pertentangan antara S&K dengan kontrak apa pun yang Anda miliki dengan Area Kerja, Ketentuan ini yang 
+                            akan diutamakan (kecuali dinyatakan lain).
+                        </li>
+                    </ol>
+                </div>
+
+                <div class="col-lg-10" style="overflow:hidden; color: black">
+                    <h2 style="font-weight: 50px; color: rgb(0, 0, 0); padding-top: 15px; padding-bottom: 5px" class=" text-left ">
+                        a. Registrasi
+                    </h2>
+                    <ul class= "a" style= "margin-left: 40px">
+
+                        <li style= "font-size: 15px" >Untuk mendaftarkan akun pada Layanan, anda harus berusia 18 tahun ke atas.
+                        </li>
+
+                        <li style= "font-size: 15px" >Data-data yang anda masukkan untuk proses registrasi harus merupakan data tepat dan lengkap.
+                        </li>
+
+                        <li style= "font-size: 15px" >Anda diperbolehkan mendaftar atas nama pribadi atau perusahaan tempat anda bekerja (apabila Anda memiliki 
+                            hak), dan tidak diperbolehkan mendaftar atas nama orang atau perusahaan lain.
+                        </li style= "font-size: 15px" >
+
+                        <li style= "font-size: 15px" >Anda bertanggung jawab untuk menjaga kerahasiaan data yang Anda gunakan untuk masuk ke akun Anda.
+                        </li>
+
+                        <li style= "font-size: 15px" >Dalam hal Anda lupa kata sandi akun, Anda dapat mengikuti langkah-langkah yang terdapat pada Layanan 
+                            untuk membuat kata sandi baru. Apabila terdapat langkah tertentu yang tidak dapat Anda penuhi, maka anda 
+                            tidak akan mendapatkan akses untuk membuat kata sandi baru.</li>
+
+                        <li style= "font-size: 15px" >Jika kami memiliki alasan untuk meyakini bahwa akan terjadi pelanggaran keamanan atau penyalahgunaan akun 
+                            Anda oleh pihak yang tidak berhak, kami akan meminta Anda untuk mengubah kata sandi Anda atau kami dapat 
+                            menangguhkan akun Anda demi keamanan.</li>
+                    
+                    </ul>
+                </div>
+
+                <div class="col-lg-10" style="overflow:hidden; color: black">
+                    <h2 style="font-weight: 50px; color: rgb(0, 0, 0); padding-top: 30px; padding-bottom: 10px" class=" text-left ">b. Kekayaan Intelektual</h2>
+                    <ul class= "a" style= "margin-left: 40px">
+
+                        <li style= "font-size: 15px">Aplikasi, situs, akun media sosial, dan semua layanan yang dimiliki AreaKerja adalah milik eksklusif AreaKerja 
+                            atau pemberi lisensinya.
+                        </li>
+
+                        <li style= "font-size: 15px">AreaKerja dan Layanan dilindungi oleh hak cipta, merek dagang, dan hak kekayaan intelektual lainnya.
+                        </li>
+
+                        <li style= "font-size: 15px">Anda dapat menggunakan dan menikmati layanan dan tampilan Layanan. Anda tidak boleh mereproduksi, 
+                            memodifikasi, menyalin atau mendistribusikan atau menggunakan untuk tujuan komersial hal apa pun di 
+                            Layanan, tanpa izin tertulis dari AreaKerja.
+                        </li>
+
+                        <li style= "font-size: 15px">Setiap pengguna Layanan setuju untuk tidak menggunakan Layanan dengan cara apa pun yang melanggar hak 
+                            kekayaan intelektual atau hak milik orang lain seperti:
+                        </li>
+
+                    </ul>
+
+                    <ol class= "d" style= "margin-left: 65px">
+
+                        <li style= "font-size: 15px">mencetak, mengunduh, menggandakan, mengirimkan atau menyalin, mereproduksi, mendistribusikan ulang, 
+                            menerbitkan ulang, atau menggunakan informasi pribadi apapun tentang pengguna lain, kecuali telah 
+                            mendapatkan izin tertulis atau dinyatakan lain pada S&K
+                        </li>
+
+                        <li style= "font-size: 15px">merekayasa balik (reverse engineer) atau mendekompilasi bagian mana pun dari Layanan;
+                        </li>
+
+                        <li style= "font-size: 15px">membuat akun dengan identitas orang lain, menggunakan nama perusahaan secara tanpa hak, atau 
+                            mengunggah lowongan pekerjaan secara tanpa hak;
+                        </li>
+
+                        <li style= "font-size: 15px">menghapus atau mengubah materi apa pun yang diunggah oleh orang atau entitas lain mana pun;
+                        </li>
+
+                        <li style= "font-size: 15px">mengakses data yang tidak ditujukan untuk Anda atau masuk ke server atau akun yang tidak diizinkan untuk 
+                            Anda akses; dan
+                        </li>
+
+                        <li style= "font-size: 15px">hal-hal lain yang dilakukan secara tanpa hak dan dapat menimbulkan kerugian secara materiil atau pun 
+                            immateriil bagi pihak lain.
+                        </li>
+
+                    </ol>
+
+                    <ul class= "a" style= "margin-left: 40px">
+
+                        <li style= "font-size: 15px">Kami tidak mengklaim kepemilikan konten Pengguna yang diposting pada Layanan dan Anda bebas untuk 
+                            berbagi konten Anda dengan orang lain pada media yang telah Kami sediakan di Layanan. Namun, Kami 
+                            memerlukan izin hukum tertentu dari Pengguna (dikenal sebagai "lisensi") untuk menyediakan Layanan. Saat 
+                            Pengguna membagikan, memposting, atau mengunggah konten yang dilindungi oleh hak kekayaan intelektual 
+                            (seperti foto atau video) pada atau sehubungan dengan Layanan Kami, Pengguna dengan ini memberikan 
+                            kepada Kami konten non-eksklusif, bebas royalti, dapat dialihkan, disublisensikan, lisensi di seluruh dunia untuk 
+                            menggunakan, mendistribusikan, memodifikasi, menjalankan, menyalin, dan menampilkan secara publik. Lisensi 
+                            ini akan berakhir ketika konten Pengguna tersebut dihapus dari sistem Kami.
+                        </li>
+
+                        <li style= "font-size: 15px">Segala konten yang terdapat pada Layanan tersimpan dalam bentuk elektronik atau dicetakkan dalam satu 
+                            salinan untuk penggunaan pribadi dan non-komersial bagi Pengguna, asalkan Pengguna menjaga semua hak 
+                            kekayaan intelektual yang terdapat didalamnya. Pengguna tidak boleh mereproduksi, memodifikasi, menyalin 
+                            atau mendistribusikan atau menggunakan untuk tujuan komersial atau terhadap setiap konten pada Layanan tanpa izin tertulis dari Kami.
+                        </li>
+
+                        <li style= "font-size: 15px; padding-bottom: 125px">Dengan menggunakan Layanan, Pengguna (khususnya Perusahaan) bersedia memberikan lisensi atas merek 
+                            dan logo miliknya kepada AreaKerja sepanjang digunakan untuk tujuan-tujuan promosional dengan cara yang 
+                            tidak melanggar hukum yang berlaku di Indonesia.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
