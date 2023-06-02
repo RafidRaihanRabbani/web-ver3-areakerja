bdb730640ada41dec294213daf985723e606b3e2
RafidRaihanRabbani <96913996+RafidRaihanRabbani@users.noreply.github.com>
2023-06-02 08:31:43 +0700

    Add files via upload

---
+++ resources/views/profil/Berhasil.blade.php
@@ -0,0 +1,26 @@
+<div class="modal fade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <img src="{{ asset('img/icon _checkmark circle 3.png') }}">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="" style="margin-bottom: 30px"></div>
+                
+                <p style="font-size: 20px; text-align:center; color: black">Edit Profil Berhasil</p>
+            </div>
+            <div class="modal-footer">
+                <div class="col-lg-4 ">
+                    <div class="footer-social">
+                        <button type="button" class="login100-form-btn">OKE</button>
+                    </div>
+                </div>
+
+                <div class="text-center">
+                    <img src="{{ asset('img/2871768.png') }}">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
