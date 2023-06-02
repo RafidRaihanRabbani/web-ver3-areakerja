a3dc36e085a4f25d6ca2c297a0dfcd9619b2ecac
RafidRaihanRabbani <96913996+RafidRaihanRabbani@users.noreply.github.com>
2023-06-02 08:36:33 +0700

    Add files via upload

---
+++ resources/views/recruitment/loadmore.blade.php
@@ -0,0 +1,216 @@
+@extends('user.main')
+@section('meta')
+<meta property="og:type" content="website">
+<meta property="og:url" content="{{ url()->current() }}">
+<meta property="og:title" content=" - Area Kerja">
+<meta property="og:description" content="Platform lowongan kerja no. 1 untuk mendapatkan talenta terbaik bagi perusahaan anda.">
+<meta property="og:image" content="{{ asset('img/img-01(2).png') }}">
+<meta name="description" content="Areakerja adalah perusahaan berbasis teknologi informasi yang berpusat di Yogyakarta. Perusahaan ini berfokus pada platform untuk mencari lowongan kerja di Daerah Istimewa Yogyakarta (DIY).">
+<meta name="keywords" content="Tentang Kami">
+@endsection
+@section('home')
+    <section class="relative" id="home">
+        <div class="banner-area2">  </div>
+    </section>
+@endsection
+@section('aboutus')
+    <section class="relative">
+
+        <div class="col" style="padding-bottom: 20pt;">
+            <h4 class="md-7 mb-1 ml-auto mr-auto"
+                style= "font-size: 20pt; padding-top: 30px" > Recruitment
+            </h4>
+            <p class="md-7 mb-1 ml-auto mr-auto"
+                style="font-size: 15pt; font-style: bold; text-align: justify;">
+                These are the people who work every day to create great products that will help you in your work.
+            </p>
+        </div>
+
+        <div class= "Topup" style="padding-bottom: 20pt;">
+            <button class="login100-form-btn" type="submit">
+                Top up
+            </button>
+        </div>
+
+        <div class="row">
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                  <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">   
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                  <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">   
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">   
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">   
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+        </div>
+
+    </section>
+@endsection
---
+++ resources/views/recruitment/recruit.blade.php
@@ -0,0 +1,281 @@
+@extends('user.main')
+@section('meta')
+<meta property="og:type" content="website">
+<meta property="og:url" content="{{ url()->current() }}">
+<meta property="og:title" content=" - Area Kerja">
+<meta property="og:description" content="Platform lowongan kerja no. 1 untuk mendapatkan talenta terbaik bagi perusahaan anda.">
+<meta property="og:image" content="{{ asset('img/img-01(2).png') }}">
+<meta name="description" content="Areakerja adalah perusahaan berbasis teknologi informasi yang berpusat di Yogyakarta. Perusahaan ini berfokus pada platform untuk mencari lowongan kerja di Daerah Istimewa Yogyakarta (DIY).">
+<meta name="keywords" content="Tentang Kami">
+@endsection
+@section('home')
+    <section class="relative" id="home">
+        <div class="banner-area2">  </div>
+    </section>
+@endsection
+@section('aboutus')
+    <section class="relative">
+
+        <div class="col">
+            <div class="row">
+                <div class="text-left">
+                    <div class="mt-10 ml-auto mr-5">
+                        <h1 class="mb-4 mt-5 ml-auto mr-auto" style ="font-size: 10pt; color: orange">Recruitment</h1>
+                        <p class="mt-9 md-4 ml-auto mr-auto"
+                            style="font-size: 20pt; font-style: bold; margin: auto 20px; text-align: left; color:black; padding-buttom: 20px">
+                            Ayo Recruit Tim Kamu 
+                            Lewat Area Kerja 
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="beranda100-pic">
+                    <img src="{{ asset('img/beranda2.png') }}" alt="IMG" style= "padding-bottom: 30px">
+                </div>
+            </div>
+        </div>    
+
+        
+        <div class= "row">
+            <div class="col s1"><img src="{{ asset('img/Illustration.png') }}" style="padding-left: 120px"></div>
+            <div class="col s1">
+                <h3 class="mb-3 ml-1" style="padding-right: 30px">Flexible Hours</h3>
+                <p class="md-5 mb-3 mr-auto "
+                    style="font-size: 15pt; font-style: bold; text-align:  justify; text-indent: 7%; padding-right: 30px ">
+                    Disini area kerja sudah mempersiapkan yang sudah siap kerja bukan yang butuh kerja 
+                </p>
+            </div>
+            <div class="col s1"><img src="{{ asset('img/018.png') }}" style="padding-left: 120px"></div>
+            <div class="col s1">
+                <h4 class="md-7 mb-1 ml-auto mr-auto"
+                    style= "font-size: 20pt">Work And Travel
+                </h4>
+                <p class="md-7 mb-1 ml-auto mr-auto"
+                    style="font-size: 15pt; font-style: bold; text-align: justify; text-indent: 7%; padding-right: 30px  ">
+                    Dapatkan kandidat dengan harga yang terjangkau
+                </p>
+            </div>
+        </div>
+
+        <div class="col">
+            <h4 class="md-7 mb-1 ml-auto mr-auto"
+                style= "font-size: 20pt;" >List Kandidat yang Kamu Undang
+            </h4>
+            <p class="md-7 mb-1 ml-auto mr-auto"
+                style="font-size: 15pt; font-style: bold; text-align: justify; padding-buttom: 30px">
+                These are the people who work every day to create great products that will help you in your work.
+            </p>
+        </div>
+
+        <div class="row">
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>UI/UX Designer</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">   
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                  <h4>HR</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div class="container-login100-form-btn">
+            <button class="login100-form-btn" type="submit">
+                Load More
+            </button>
+        </div>
+
+        <div class="col">
+            <h4 class="md-7 mb-1 ml-auto mr-auto"
+                style= "font-size: 20pt; padding-top: 30px" >Recruitment
+            </h4>
+            <p class="md-7 mb-1 ml-auto mr-auto"
+                style="font-size: 15pt; font-style: bold; text-align: justify;">
+                These are the people who work every day to create great products that will help you in your work.
+        
+            </p>
+        </div>
+
+
+        <div class="row">
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                  <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">   
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                  <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+            <div class="column">
+              <div class="cardrecruit">   
+                <img src="{{ asset('img/img-01(2).png') }}" alt="Avatar" style="width:50%">
+                <div class="container">
+                    <h4>Project Manager</h4>
+                    <span class="txt1">
+                        Skill:
+                    </span>
+                    <a class="txt2" style= "color: orange">
+                        Competent
+                    </a>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div class="container-login100-form-btn">
+            <button class="login100-form-btn" type="submit">
+                Load More
+            </button>
+        </div>
+
+
+    </section>
+    <style>
+        .login100-form-btn {
+        font-family: Montserrat-Bold;
+        font-size: 15px;
+        line-height: 1.5;
+        color: #5b5b5b;
+        text-transform: uppercase;
+    
+        width: 20%;
+        height: 50px;
+        border-radius: 25px;
+        background: #E8E9ED;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -moz-box;
+        display: -ms-flexbox;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0 25px;
+    
+    }
+    
+    .login100-form-btn:hover {
+        font-family: Montserrat-Bold;
+        font-size: 15px;
+        line-height: 1.5;
+        color: #ffffff;
+        text-transform: uppercase;
+    
+        width: 25%;
+        height: 50px;
+        border-radius: 25px;
+        background: #ff8400;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -moz-box;
+        display: -ms-flexbox;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0 25px;
+    
+        -webkit-transition: all 0.4s;
+        -o-transition: all 0.4s;
+        -moz-transition: all 0.4s;
+        transition: all 0.4s;
+    }
+    </style>
+@endsection
