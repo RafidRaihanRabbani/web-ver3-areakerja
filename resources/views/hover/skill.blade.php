d904e52c02ffc9153d6f247432bf7c94ee9d7293
RafidRaihanRabbani <96913996+RafidRaihanRabbani@users.noreply.github.com>
2023-06-02 07:17:23 +0700

    Add files via upload

---
+++ resources/views/hover/skill.blade.php
@@ -0,0 +1,45 @@
+<meta property="og:type" content="website">
+<meta property="og:url" content="{{ url()->current() }}">
+<meta property="og:title" content="{{ $title }} - Area Kerja">
+<meta property="og:description" content="Platform lowongan kerja no. 1 untuk mendapatkan talenta terbaik bagi perusahaan anda.">
+<meta property="og:image" content="{{ asset('img/img-01.png') }}">
+<meta name="description" content="Areakerja adalah perusahaan berbasis teknologi informasi yang berpusat di Yogyakarta. Perusahaan ini berfokus pada platform untuk mencari lowongan kerja di Daerah Istimewa Yogyakarta (DIY).">
+<meta name="keywords" content="Tentang Kami">
+@endsection
+
+@section('aboutus')
+    <section class="relative">
+        <div class="container text-center">
+            <div class="col-12 col-sm-8 mb-5 ml-auto mr-auto form-wrap-main">
+                <h1 class="mb-3 mt-3"><strong>Apa itu level skill</strong></h1>
+                <p class="mb-3"
+                    style="font-size: 13pt; font-style: bold; margin: auto 20px; text-align: justify; text-indent: 7%;">
+                    Pertama, Level <strong>Competent</strong>. Di level ini, seorang praktisi  
+                    memahami konteks, kapan sebuah teknik efektif 
+                    dilakukan dan kapan sebuah teknik tidak efektif 
+                    dilakukan. Mereka secara sadar mampu menganalisis 
+                    teknik yang mereka gunakan. <strong>(Biasanya 
+                    berpengalaman dalam bidangnya selama 1 tahun)</strong>
+                </p>
+                <p class="mb-3"
+                    style="font-size: 13pt; font-style: bold; margin: auto 20px; text-align: justify; text-indent: 7%;">
+                    Kedua, level <strong>Proficient</strong> (Cakap). Di level ini, seorang 
+                    praktisi  memahami prinsip-prinsip (kaidah) di balik 
+                    teknik yang mereka gunakan. Sehingga mereka lebih 
+                    fleksibel dalam menggunakan teknik-teknik yang 
+                    mereka kuasai. <strong>(Biasanya berpengalaman dalam 
+                    bidangnya selama 2 tahun)</strong>
+
+                </p>
+                <p class="mb-3"
+                    style="font-size: 13pt; font-style: bold; margin: auto 20px; text-align:  justify; text-indent: 7%; ">
+                    Ketiga, level Expert (Ahli). Di level ini, intuisi seorang 
+                    praktisi  terbentuk. Mereka mulai memahami pola-pola 
+                    yang terkait dengan keterampilan mereka. <strong>(Biasanya 
+                    berpengalaman dalam bidangnya selama 3 tahun ke atas).</strong>
+                </p>
+            </div>
+        </div>
+
+    </section>
+@endsection
