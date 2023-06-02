d206298688302c9c64f6a519106e982d2c018f25
RafidRaihanRabbani <96913996+RafidRaihanRabbani@users.noreply.github.com>
2023-06-02 07:24:42 +0700

    Add files via upload

---
+++ resources/views/kandidat/daftar.blade.php
@@ -0,0 +1,160 @@
+@extends('user.main')
+@section('meta')
+<meta property="og:type" content="website">
+<meta property="og:url" content="{{ url()->current() }}">
+<meta property="og:title" content=" - Area Kerja">
+<meta property="og:description" content="Platform lowongan kerja no. 1 untuk mendapatkan talenta terbaik bagi perusahaan anda.">
+<meta property="og:image" content="{{ asset('img/img-01(2).png') }}">
+<meta name="description" content="Areakerja adalah perusahaan berbasis teknologi informasi yang berpusat di Yogyakarta. Perusahaan ini berfokus pada platform untuk mencari lowongan kerja di Daerah Istimewa Yogyakarta (DIY).">
+<meta name="keywords" content="Tentang Kami">
+@endsection
+@section('home')
+<section class="relative" id="home">
+    <div class="banner-daftar">
+        <div class="row align-items-center" style="margin-right: 15px; margin-left: -600px; margin-bottom: 250px; margin-top: 30px;">
+            <div class="banner-content col-lg-12">
+                <h1 style="color: #ffffff; font-size: 50px; margin-right: 290px; text-shadow: 2px 2px 3px #353535b0;">
+                    <strong>Daftar</strong>
+                </h1>
+                <h2 style="color: #ffffff; font-size: 50px; margin-bottom:5px; margin-right: 220px; text-shadow: 2px 2px 3px #353535b0;">
+                    <strong>Kandidat</strong>
+                </h2>
+                <h6 style="color: #ffffff; margin-right: 155px; text-shadow: 1px 1px 2px #353535b0;">
+                    Ikuti pelatihan terakreditasi Aeakerja.com
+
+                </h6>
+                <h6 style="color: #ffffff; margin-right: 175px; text-shadow: 1px 1px 2px #353535b0;">
+                    dan dapatkan pekerjaan impian anda!
+                </h6>
+                <a href="{{ route('berandape') }}">
+                <button class="btn btn-lg mt-3" style= "margin-bottom: 600px margin-left: -290px;"
+                style="background:#fe7b54; color:white;" type="button" 
+                data-toggle="modal" data-target="#modalDaftar">Daftar</button>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+@endsection
+@section('aboutus')
+    <section class="relative" id="home">
+
+        <div class="container" style="background:#F6AE2D;">
+            <h2 style="font-weight: normal; color: white; padding-left: 500px;" class=" text-center ">Benefit Menjadi</h2>
+            <h2 style="font-weight: normal; color: white; padding-left: 550px;" class=" text-center ">Kandidat Areakerja.com</h2>
+            
+            <div class="row">
+                <div class="col-lg-4" style="overflow:hidden">
+                    <img src="{{ asset('img/daftar1.png') }}" alt="" class="img-fluid" style="margin-top:-0px; width: 300px; height: 450px; margin-left: 50px;">
+                </div>
+                <div class="col-lg-8">
+                   <ul class="h-100" style="list-style:none; display:flex; flex-direction: column; justify-content: center;">
+                       <li>
+                           <div class="d-flex align-items-center mb-3" >
+                            <h2 class="m-0 mr-2 my-auto; ">
+                                <img src="{{ asset('img/judul(7)1.png') }}" style="width: 55px; height: 55px; margin-right: 25px;">
+                            </h2>
+                            <h4 class="m-0" style="color: #ffffff; margin-left: 55px;">
+                                Menjadi prioritas pilihan dari perusahaan mitra Areakerja
+                            </h4>
+                           </div>
+                       </li>
+                       <li>
+                            <div class="d-flex align-items-center mb-3" >
+                                <h2 class="m-0 mr-2 my-auto">
+                                    <img src="{{ asset('img/judul(8)1.png') }}" style="width: 65px; height: 45px; margin-right: 25px;">
+                                </h2>
+                                <h4 class="m-0" style="color: #ffffff; ">Areakerja memiliki banyak mitra perusahaan yang sedang membuka lowongan 
+                                </h4>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="d-flex align-items-center mb-3" >
+                                <h2 class="m-0 mr-2 my-auto">
+                                    <img src="{{ asset('img/judul(9)1.png') }}" style="width: 55px; height: 55px; margin-right: 25px;">
+                                </h2>
+                                <h4 class="m-0" style="color: #ffffff; margin-left: 35px;">
+                                    Areakerja merupakan perusahaan terpercaya berbadan hukum
+                                </h4>
+                            </div>
+                        </li>
+                    </ul>
+               </div>
+            </div>
+        </div>
+
+        <div class="container" style="margin-top: 55px;">
+            <h2 style="font-weight: normal" class=" text-center ">Cara Daftar Kandidat</h2>
+            <hr class= "hrareakerja">
+            <div class="cara" >
+                <div class="col-md-12" style= "padding-above: 30px">
+                   <ul class="h-100" style="list-style:none; display:flex; flex-direction: row; justify-content: center;">
+                       <li>
+                           <div class="klik" >
+                            <h2 class="col-md-3" style="color: #ffffff; width:45px">
+                                01
+                            </h2>
+                            <h4 class="m-0" style="color: #ffffff;">
+                                Klik Daftar untuk
+                            </h4>
+                            <h4 class="m-0" style="color: #ffffff;">
+                                registrasi 
+                            </h4>
+                            <h4 class="m-0" style="color: #ffffff;">
+                                kandidat
+                            </h4>
+                           </div>
+                       </li>
+                       <li>
+                            <div class="data" >
+                                <h2 class="col-md-3" style="color: #2F4858; width:45px">
+                                    02
+                                </h2>
+                                <h4 class="m-0" style="color: #2F4858;">Lengkapi data yang
+                                </h4>
+                                <h4 class="m-0" style="color: #2F4858;">diperlukan pada
+                                </h4>
+                                <h4 class="m-0" style="color: #2F4858;">proses registrasi
+                                </h4>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="regist" >
+                                <h2 class="col-md-3o" style="color: #2F4858; width:45px">
+                                    03
+                                </h2>
+                                <h4 class="m-0" style="color: #2F4858;">
+                                    Tunggu pemberitahuan
+                                </h4>
+                                <h4 class="m-0" style="color: #2F4858;">
+                                    setelah melakukan
+                                </h4>
+                                <h4 class="m-0" style="color: #2F4858;">
+                                    registrasi
+                                </h4>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="prosedur" >
+                                <h2 class="col-md-3" style="color: #ffffff; width:45px">
+                                    04
+                                </h2>
+                                <h4 class="m-0" style="color: #ffffff;">
+                                    Ikuti pelatihan
+                                </h4>
+                                <h4 class="m-0" style="color: #ffffff;">
+                                    sesuai prosedur
+                                </h4>
+                                <h4 class="m-0" style="color: #ffffff;">
+                                    Areakerja.com
+                                </h4>
+                            </div>
+                        </li>
+                   </ul>
+               </div>
+            </div>
+        </div>
+    </section>
+@endsection
